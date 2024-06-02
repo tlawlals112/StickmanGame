e74f421cdf313a4a344bc316892989d3febeaca3
tlawlals112 <tlawlals112@gmail.com>
2024-06-02 20:47:04 +0900

    Add Dash Effect when attacking
    * Dash forward and return back

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,6 +7,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
 import javax.swing.Timer;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private Image stickmanIdle1;
@@ -18,9 +19,14 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private Image stickmanHighKick;
     private Image currentImage;
     private int stickmanX, stickmanY;
+    private int originalX;
     private Timer timer;
     private boolean isAttacking;
     private int idleIndex;
+    private Timer skillTimer;
+    private int moveAmount;
+    private int moveDirection;
+    private Random random;
 
     public GamePanel() {
         this.stickmanIdle1 = new ImageIcon("assets/stickman_idle1.png").getImage();
@@ -33,13 +39,30 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         this.currentImage = stickmanIdle1;
         this.stickmanX = 100;
         this.stickmanY = 400;
+        this.originalX = stickmanX;
         this.isAttacking = false;
         this.idleIndex = 0;
-        this.timer = new Timer(500, this); // 500 milliseconds to toggle idle state
+        this.moveAmount = 0;
+        this.moveDirection = 0;
+        this.random = new Random();
+        this.timer = new Timer(20, this); // 20 milliseconds to update movement and idle image
         this.timer.start();
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+
+        // Timer for skill effect duration
+        this.skillTimer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Reset to idle state after 1 second
+                isAttacking = false;
+                moveAmount = 0;
+                stickmanX = originalX;
+                currentImage = stickmanIdle1;
+                skillTimer.stop(); // Stop the timer after skill effect duration
+            }
+        });
     }
 
     @Override
@@ -52,21 +75,26 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
     public void actionPerformed(ActionEvent e) {
         // Toggle between idle images if no action is being performed
         if (!isAttacking) {
-            switch (idleIndex) {
+            int rand = random.nextInt(3);
+            switch (rand) {
                 case 0:
                     currentImage = stickmanIdle1;
-                    idleIndex = 1;
                     break;
                 case 1:
                     currentImage = stickmanIdle2;
-                    idleIndex = 2;
                     break;
                 case 2:
                     currentImage = stickmanIdle3;
-                    idleIndex = 0;
                     break;
             }
         }
+        
+        // Move the stickman if moveAmount is not zero
+        if (moveAmount > 0) {
+            stickmanX += moveDirection;
+            moveAmount -= Math.abs(moveDirection);
+        }
+        
         repaint();
     }
 
@@ -74,29 +102,53 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
         isAttacking = true;
+        originalX = stickmanX;
         if (code == KeyEvent.VK_LEFT) {
-            stickmanX -= 5;
+            moveDirection = -2; // Increased speed for smoother movement
+            moveAmount = 25;
         } else if (code == KeyEvent.VK_RIGHT) {
-            stickmanX += 5;
+            moveDirection = 2; // Increased speed for smoother movement
+            moveAmount = 25;
         } else if (code == KeyEvent.VK_A) {
             currentImage = stickmanJab;
+            moveDirection = 4; // Move forward for jab
+            moveAmount = 100; // Move forward for jab
+            startSkillTimer(); // Start the skill timer when using skill
         } else if (code == KeyEvent.VK_S) {
             currentImage = stickmanStraight;
+            moveDirection = 4; // Move forward for straight
+            moveAmount = 100; // Move forward for straight
+            startSkillTimer(); // Start the skill timer when using skill
         } else if (code == KeyEvent.VK_Z) {
             currentImage = stickmanLowKick;
+            moveDirection = 2; // Move forward for low kick
+            moveAmount = 50; // Move forward for low kick
+            startSkillTimer(); // Start the skill timer when using skill
         } else if (code == KeyEvent.VK_X) {
             currentImage = stickmanHighKick;
+            moveDirection = 2; // Move forward for high kick
+            moveAmount = 50; // Move forward for high kick
+            startSkillTimer(); // Start the skill timer when using skill
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        int code = e.getKeyCode();
-        if (code == KeyEvent.VK_A || code == KeyEvent.VK_S || code == KeyEvent.VK_Z || code == KeyEvent.VK_X) {
-            isAttacking = false;
-        }
+        // No need to change anything in keyReleased method
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+        // No need to change anything in keyTyped method
+    }
+
+    private void startSkillTimer() {
+        // Start the skill timer if it's not already running
+        if (!skillTimer.isRunning()) {
+            skillTimer.start();
+        } else {
+            // Restart the skill timer if it's already running
+            skillTimer.restart();
+        }
+    }
 }
